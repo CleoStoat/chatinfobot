4a177a1c3632e9c2e65a5cdc234314b764a9bb59
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-08-29 11:58:14 -0300

    Added get_list_of_users_msg_count to repository

--- adapters/repository.py
+++ adapters/repository.py
@@ -1,20 +1,24 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import config
 from domain.model import ChatMessage
 from sqlalchemy.orm import Session, session
+from sqlalchemy.sql import func
 
 
 class AbstractRepository(ABC):
-
     @abstractmethod
     def add_message(self, user_id: int, chat_id: int, time: datetime) -> None:
         raise NotImplementedError
 
     @abstractmethod
     def get_messages(self, chat_id: int) -> List[ChatMessage]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_list_of_users_msg_count(self) -> List[Tuple[int, int]]:
         raise NotImplementedError
 
 
@@ -27,7 +31,16 @@ class SqlAlchemyRepository(AbstractRepository):
     def add_message(self, user_id: int, chat_id: int, time: datetime) -> None:
         chat_msg = ChatMessage(user_id, chat_id, time)
         self.session.add(chat_msg)
-        
+
     def get_messages(self, chat_id: int) -> List[ChatMessage]:
-        return self.session.query(ChatMessage).filter_by(chat_id = chat_id).all()
-        
+        return self.session.query(ChatMessage).filter_by(chat_id=chat_id).all()
+
+    def get_list_of_users_msg_count(self) -> List[Tuple[int, int]]:
+        return (
+            self.session.query(
+                ChatMessage.user_id,
+                func.count(ChatMessage.time),
+            )
+            .group_by(ChatMessage.user_id)
+            .all()
+        )
