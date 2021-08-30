72af040bacd0b23c23fda498fd499b41f565ef7b
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2021-08-30 02:33:25 -0300

    Added plot_line function

--- service_layer/plotting.py
+++ service_layer/plotting.py
@@ -20,3 +20,21 @@ def plot_hor_bars(
     ax.set_title(title)
 
     return fig
+
+def plot_line(
+    x_labels: List[int], heights: List[Union[int, float]], x_label: str, y_label: str, title: str
+) -> Figure:
+    fig, ax = pyplot.subplots()
+    fig.set_size_inches(10, 10)
+    x_pos = numpy.arange(len(x_labels))
+
+    # ax.barh(y_pos, widths, align="center")
+    ax.plot(x_labels, heights)
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(x_labels)
+    # ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+
+    return fig
